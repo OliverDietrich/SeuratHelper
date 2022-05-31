2f13ba657ccf3f0abe931fdd3ec6ec1eed6361c6
Oliver Dietrich <Oliver.Dietrich@helmholtz-hiri.de>
2022-05-31 13:39:58 +0200

    Change file permissions.

--- R/anndata.R
+++ R/anndata.R
@@ -1,10 +1,10 @@
-################################################################################
 # Helper functions for interoperability between Seurat and anndata
 
 #' Read anndata object (.h5ad) and convert to Seurat object on disk
 #' 
 #' @param file Path to h5ad object
 #' @returns Seurat object
+#' @export
 #' @example
 #' ds <- read_h5ad("~/Downloads/pbmc3k.h5ad)
 #' 
@@ -61,6 +61,7 @@ read_h5ad <- function(file) {
 #' 
 #' @param object Seurat object
 #' @param file Path to h5ad object (e.g. "~/Downloads/pbmc3k.h5ad")
+#' @export
 #' @example 
 #' write_h5ad(ds, "~/Documents/pbmc3k.h5ad")
 #' 
@@ -75,8 +76,11 @@ write_h5ad <- function(object, file) {
   print("Begin conversion")
   
   # Add count matrix
+  assay <- Seurat::DefaultAssay(object)
   rhdf5::h5write(
-    obj = Matrix::as.matrix(ds@assays$RNA@data), file = file, name = "X"
+    obj = Matrix::as.matrix(
+      object[[assay]]@data
+      ), file = file, name = "X"
       )
   
   # Add coldata (obs)
@@ -99,75 +103,3 @@ write_h5ad <- function(object, file) {
   }
   print("Done.")
 }
-
-# end of document
-################################################################################
-
-# TRIAL ZONE
-
-path <- list(
-  pbmc3k = "/home/oliver/Projects/Robert_Strasser/data/pbmc3k.h5ad",
-  vieira = "/home/oliver/Projects/Robert_Strasser/data/vieira19.h5ad",
-  lukassen = "/home/oliver/Projects/Robert_Strasser/data/lukassen20.h5ad",
-  to = "/home/oliver/Downloads/object.h5ad"
-)
-
-from <- path$vieira
-to <- "/home/oliver/Downloads/object.h5ad"
-
-rhdf5::h5ls(from, all = T)
-
-# Fetch coldata from h5ad
-obs <- rhdf5::h5read(from, "obs", read.attributes = TRUE)
-if (class(obs) == "list") {
-  obs_attr <- attributes(obs)
-  lvls <- rhdf5::h5read(from, "obs")[["__categories"]]
-  obs_names <- rhdf5::h5read(from, "obs")[[obs_attr[["_index"]]]]
-  obs <- as.data.frame(obs[obs_attr$`column-order`], row.names = obs_names
-  )
-  for (i in names(lvls)) {
-    lv <- lvls[[i]]
-    names(lv) <- sort(unique(obs[[i]])) + 1
-    obs[[i]] <- factor(as.character(lv[obs[[i]] + 1]), levels = lvls[[i]])
-  }
-} else {
-  if (class(obs) == "data.frame") {
-    rownames(obs) <- obs$index
-    obs <- obs[, -which(names(obs) == "index")]
-  } else {
-    stop("Unknown class of 'obs'")
-  }
-}
-
-
-# Create new h5ad file
-file.remove(to)
-rhdf5::h5createFile(to)
-
-# Add data.frame complete
-lvls <- list()
-for (i in names(obs)) {
-  if (class(obs[[i]]) == "factor") {
-    lvls[[i]] <- levels(obs[[i]])
-    obs[[i]] <- as.character(obs[[i]])
-  }
-}
-obs <- cbind(data.frame(index = row.names(obs)), obs)
-rhdf5::h5write(obs, to, "obs")
-
-
-
-################################################################################
-# Convert obs data frame to list
-nobs <- list(
-  lapply(obs, levels)[!sapply(lapply(obs, levels), is.null)], obs_names
-)
-names(nobs) <- c("__categories", obs_attr[["_index"]])
-obs <- as.list(obs)
-for (i in names(obs)) {
-  obs[[i]] <- as.double(obs[[i]]) -1
-  attr(obs[[i]], "dim") <- length(obs[[i]])
-}
-obs_attr$`column-order` <- names(obs)
-obs <- c(nobs, obs)
-attributes(obs) <- obs_attr
